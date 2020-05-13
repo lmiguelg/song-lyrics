d6edf8b806bf9c7c101fd12b89912b381cfa1d68
lmiguelg <lmiguelgoncalves97@hotmail.com>
2020-05-14 00:42:09 +0100

    Updates

---
+++ precache-manifest.c99ee07c5a1321bbcf121553a8eb3aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808e3f0d7543ee306b47bcf37a3ee489",
+    "url": "/song-lyrics/index.html"
+  },
+  {
+    "revision": "980f72193d3583f41d57",
+    "url": "/song-lyrics/static/css/main.2a9518dd.chunk.css"
+  },
+  {
+    "revision": "639701e6c7c698312777",
+    "url": "/song-lyrics/static/js/2.6b019a85.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/song-lyrics/static/js/2.6b019a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980f72193d3583f41d57",
+    "url": "/song-lyrics/static/js/main.f87e8946.chunk.js"
+  },
+  {
+    "revision": "323f6b02f7b7067c491c",
+    "url": "/song-lyrics/static/js/runtime-main.fda009b8.js"
+  }
+]);
